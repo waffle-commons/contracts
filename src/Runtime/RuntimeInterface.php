ca66458a371ea85c67594ffdc121ffff0628ecf1
Leslie Petrimaux <leslypetrimaux@gmail.com>
2025-11-26 14:54:00 +0100

    update `runtime` interface

--- src/Runtime/RuntimeInterface.php
+++ src/Runtime/RuntimeInterface.php
@@ -4,9 +4,15 @@ declare(strict_types=1);
 
 namespace Waffle\Commons\Contracts\Runtime;
 
+use Psr\Http\Message\ServerRequestInterface;
 use Waffle\Commons\Contracts\Core\KernelInterface;
+use Waffle\Commons\Contracts\Http\ResponseEmitterInterface;
 
 interface RuntimeInterface
 {
-    public function run(KernelInterface $kernel): void;
+    public function run(
+        KernelInterface $kernel,
+        ServerRequestInterface $request,
+        ResponseEmitterInterface $emitter
+    ): void;
 }
