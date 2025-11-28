596f3301d444d317af825a9c2a83231e29a9e454
Leslie Petrimaux <leslypetrimaux@gmail.com>
2025-11-28 16:08:57 +0100

    implement `routing` component

--- src/System/SystemInterface.php
+++ src/System/SystemInterface.php
@@ -5,11 +5,8 @@ declare(strict_types=1);
 namespace Waffle\Commons\Contracts\System;
 
 use Waffle\Commons\Contracts\Core\KernelInterface;
-use Waffle\Router\Router;
 
 interface SystemInterface
 {
     public function boot(KernelInterface $kernel): self;
-
-    public function registerRouter(Router $router): void;
 }
